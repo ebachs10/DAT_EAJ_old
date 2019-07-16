37dfd1fa1ca298d8270e24f376b317e3f086d338
Esben Bach Soerensen <ebachs10@gmail.com>
2019-07-16 13:57:35 +0200

    Added Employee app.

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import EmployeeCreationForm, EmployeeChangeForm
+from .models import Employee
+
+class EmployeeAdmin(UserAdmin):
+    add_form    = EmployeeCreationForm
+    form        = EmployeeChangeForm
+    model       = Employee
+    list_display = ['email' , 'username', 'age', 'is_staff',]
+
+admin.site.register(Employee,EmployeeAdmin)
---
+++ employees/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import Employee
+
+class EmployeeCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = Employee
+        fields = UserCreationForm.Meta.fields + ('age',)
+
+class EmployeeChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Employee
+        fields = UserChangeForm.Meta.fields
