2704e6d1705ee287d19cac859a0f5f1e322f62b3
Esben Bach Soerensen <ebachs10@gmail.com>
2019-07-20 19:49:17 +0200

    Working on chapter 14.

--- journals/views.py
+++ journals/views.py
@@ -1,3 +1,5 @@
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -7,22 +9,39 @@ from .models import Journal
 class JournalListView(ListView):
     model = Journal
     template_name = 'journal_list.html'
+    login_url = 'login'
 
-class JournalCreateView(CreateView):
+class JournalCreateView(LoginRequiredMixin, CreateView):
     model = Journal
     template_name = 'journal_new.html'
-    fields = ('AnimalType' , 'TagNumber' , 'disease' , 'note' , 'author')
+    fields = ('AnimalType' , 'TagNumber' , 'disease' , 'note')
+    login_url = 'login'
 
-class JournalDetailView(DetailView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class JournalDetailView(LoginRequiredMixin, DetailView):
     model = Journal
     template_name = 'journal_detail.html'  
+    login_url = 'login'
 
-class JournalUpdateView(UpdateView):
+class JournalUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Journal
     fields = ('TreatmentStatus', 'note',)
-    template_name = 'journal_edit.html'     
+    template_name = 'journal_edit.html'  
+    login_url = 'login'  
 
-class JournalDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user         
+
+class JournalDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Journal
     template_name = 'journal_delete.html'
     success_url = reverse_lazy('journal_list')
+    login_url = 'login'    
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
